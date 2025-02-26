56e99de9bacef290b4e6e2b13900cb9a15678f78
QuiLion7 <quilbrub@gmail.com>
2025-02-26 00:04:21 -0300

    fix: component update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,33 +3,16 @@
 import SocialMediaLinks from "./social-links";
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex flex-col items-center justify-center gap-4 py-6 md:h-16 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built by{" "}
-            <a
-              href="https://github.com/quilantera"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              QuiL
-            </a>
-            . The source code is available on{" "}
-            <a
-              href="https://github.com/quilantera/quildetection"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              GitHub
-            </a>
-            .
-          </p>
+      <div className="container w-full flex flex-col items-center justify-center gap-2 py-6 md:h-16 md:py-0">
+        <div className="">
+          <SocialMediaLinks />
         </div>
-        <SocialMediaLinks />
+        <p className="text-xs text-muted-foreground">
+          © {currentYear} Quilion Oliveira. Todos os direitos reservados.
+        </p>
       </div>
     </footer>
   );
